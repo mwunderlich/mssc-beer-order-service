af789339e1a73c17903fb3fe9897e84e9c4f6b68
mwunderlich <martin@wunderlich.com>
2020-07-09 13:03:46 +0200

    Add wiremock, set up integration test.

---
+++ src/main/java/guru/sfg/brewery/model/BeerPagedList.java
@@ -0,0 +1,19 @@
+package guru.sfg.brewery.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDto> {
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+        // TODO Auto-generated constructor stub
+    }
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+        // TODO Auto-generated constructor stub
+    }
+}
---
+++ src/test/java/guru/sfg/beer/order/service/services/BeerOrderManagerImplIntegrationTest.java
@@ -0,0 +1,117 @@
+package guru.sfg.beer.order.service.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jenspiegsa.wiremockextension.WireMockExtension;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderLine;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.repositories.CustomerRepository;
+import guru.sfg.beer.order.service.services.beer.BeerServiceImpl;
+import guru.sfg.brewery.model.BeerDto;
+import guru.sfg.brewery.model.BeerPagedList;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Integration test (IT)
+ *
+ * @author Martin Wunderlich
+ */
+@ExtendWith(WireMockExtension.class)
+@SpringBootTest
+public class BeerOrderManagerImplIntegrationTest {
+
+    @Autowired
+    BeerOrderManager beerOrderManager;
+
+    @Autowired
+    BeerOrderRepository beerOrderRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    WireMockServer wireMockServer;
+
+    Customer testCustomer;
+
+    UUID beerId = UUID.randomUUID();
+
+    @TestConfiguration
+    static class RestTemplateBuilderProvider {
+
+        @Bean(destroyMethod = "stop")
+        public WireMockServer wireMockServer() {
+            WireMockServer server = with(wireMockConfig().port(8083));
+            server.start();
+
+            return server;
+        }
+    }
+
+    @Before
+    public void setup() {
+        testCustomer = customerRepository.save(Customer.builder()
+                .customerName("Test customer")
+                .build());
+
+    }
+
+    @Test
+    void testNewToAllocate() throws JsonProcessingException {
+        BeerDto beerDto = BeerDto.builder().id(beerId).upc("12345").build();
+        BeerPagedList list = new BeerPagedList(Collections.singletonList(beerDto));
+
+        wireMockServer.stubFor(get(BeerServiceImpl.BEER_UPC_PATH_V1 + "12345")
+                .willReturn(okJson(objectMapper.writeValueAsString(list))));
+
+        BeerOrder beerOrder = createBeerOrder();
+
+        BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
+
+        assertNotNull(savedBeerOrder);
+        assertEquals(BeerOrderStatusEnum.ALLOCATED, savedBeerOrder.getOrderStatus());
+    }
+
+    private BeerOrder createBeerOrder(){
+        BeerOrder beerOrder = BeerOrder.builder()
+                .customer(testCustomer)
+                .build();
+
+        Set<BeerOrderLine> lines = new HashSet<>();
+        lines.add(BeerOrderLine.builder()
+                .beerId(beerId)
+                .upc("12345")
+                .orderQuantity(1)
+                .beerOrder(beerOrder)
+                .build());
+
+        beerOrder.setBeerOrderLines(lines);
+
+        return beerOrder;
+    }
+}
